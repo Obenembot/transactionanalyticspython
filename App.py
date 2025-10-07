5b16d6702888d93cd14aeb7f55a169eb395ad4d3
Sampson Obenembot <sampson.obenembot@hellogroup.co.za>
2025-10-07 21:49:26 +0200

    init

--- App.py
+++ App.py
@@ -1,1 +1,23 @@
-print("Welcome to Transaction Analytics !")
+import pandas as pandas # Required for data manipulation
+import openpyxl as openpyxl # Required for Excel file handling
+import streamlit as streamlit # Streamlit for web app
+
+# Load data from file
+file_path = "predictCustomer.xlsx"
+df = pandas.read_excel(file_path)
+
+# Convert Transaction Date to datetime
+df["TransactionDate"] = pandas.to_datetime(df["TransactionDate"])
+
+
+#  Set the sidebar for navigation
+streamlit.sidebar.title("Navigation")
+page = streamlit.sidebar.radio("Go to", ["Overview", "Transaction Analytics"])
+
+
+if page == "Overview":
+    streamlit.title("Overview Dashboard")
+
+    streamlit.dataframe(df)
+    # streamlit.write(df.groupby("HpId").size())
+# print("Welcome to Transaction Analytics !", df)
