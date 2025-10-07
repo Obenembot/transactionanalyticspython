112e3dc8ed86cd18829af7fd244e1174992397b3
Sampson Obenembot <sampson.obenembot@hellogroup.co.za>
2025-10-07 22:13:29 +0200

    init v2

--- App.py
+++ App.py
@@ -1,5 +1,5 @@
 import pandas as pandas # Required for data manipulation
-# import openpyxl as openpyxl # Required for Excel file handling
+import openpyxl as openpyxl # Required for Excel file handling
 import streamlit as streamlit # Streamlit for web app
 
 # Load data from file
@@ -9,7 +9,7 @@ df = pandas.read_excel(file_path)
 # Convert Transaction Date to datetime
 df["TransactionDate"] = pandas.to_datetime(df["TransactionDate"])
 
-
+print(openpyxl)
 #  Set the sidebar for navigation
 streamlit.sidebar.title("Navigation")
 page = streamlit.sidebar.radio("Go to", ["Overview", "Transaction Analytics"])
